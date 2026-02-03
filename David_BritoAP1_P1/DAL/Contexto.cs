162aca27f907b1adb0e921409c65c5b777b0d7b6
MrPapuNoob <yostinbrito@hotmail.com>
2026-02-02 21:06:54 -0400

    Agrega el contexto

---
+++ David_BritoAP1_P1/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using David_BritoAP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace David_BritoAP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+
+    }
+
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
+
